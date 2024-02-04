2660a6583284f73d6a69d5d4d113cd88d980ca8f
philbindanielross <philbindanielr@gmail.com>
2024-02-04 14:58:56 -0600

    Added Pokemon list
    Added 5 different Pokemon to the Pokemon list, with name, heigh, attack, defense, speed, and a type array of two.

--- JS/scripts.js
+++ JS/scripts.js
@@ -1,4 +1,54 @@
 // Java Script
-alert("Hello world");
-let favoriteFood = "Ethiopian";
-document.write(favoriteFood);
+// alert("Hello world");
+// let favoriteFood = "Ethiopian";
+// document.write(favoriteFood);
+let pokemonList = [
+  {
+    name: "Ivysaur",
+    height: "1",
+    attack: "62",
+    defense: "63",
+    speed: "60",
+    types: ["grass", "poison"],
+  },
+  {
+    name: "Balbasaur",
+    height: "0.7",
+    attack: "49",
+    defense: "49",
+    speed: "45",
+    types: ["grass", "poison"],
+  },
+  {
+    name: "Squirtle",
+    height: "0.5",
+    attack: "48",
+    defense: "65",
+    speed: "43",
+    types: ["water"],
+  },
+  {
+    name: "Charizard",
+    height: "1.7",
+    attack: "84",
+    defense: "78",
+    speed: "100",
+    types: ["fire", "flying"],
+  },
+  {
+    name: "Beedrill",
+    height: "1",
+    attack: "90",
+    defense: "40",
+    speed: "75",
+    types: ["bug", "poison"],
+  },
+  {
+    name: "Paras",
+    height: "0.3",
+    attack: "70",
+    defense: "55",
+    speed: "25",
+    types: ["grass", "bug"],
+  },
+];
