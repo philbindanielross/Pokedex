1696fd59a47232cb6591a351ccf8428a52330b85
philbindanielross <philbindanielr@gmail.com>
2024-03-03 14:35:32 -0600

    add the addListItem function
    Changed the forEach loop so it added a list item, rather than using the document.write function. I also made some styling changes and added an event listener.

--- JS/scripts.js
+++ JS/scripts.js
@@ -55,24 +55,41 @@ const pokemonRepository = (function () {
   function getAll() {
     return pokemonList;
   }
+  function addListItem(pokemon) {
+    let ListOfPokemon = document.querySelector(".pokemon-list");
+    let listItem = document.createElement("li");
+    let button = document.createElement("button");
+    button.innerText = pokemon.name;
+    button.classList.add("pokemon-item");
+    listItem.appendChild(button);
+    ListOfPokemon.appendChild(listItem);
+    button.addEventListener("click", function (event) {
+      showDetails(pokemon);
+    });
+  }
+  function showDetails(pokemon) {
+    console.log(pokemon);
+  }
   return {
     add: add,
     getAll: getAll,
+    addListItem: addListItem,
   };
 })();
 
 pokemonRepository.getAll().forEach(function (pokemon) {
-  if (pokemon.height >= 1.5) {
-    document.write(
-      `${pokemon.name}: ${pokemon.height} meters tall. It is a big Pokemon. <br>`
-    );
-  } else if (pokemon.height <= 0.5) {
-    document.write(
-      `${pokemon.name}: ${pokemon.height} meters tall. It is a small Pokemon. <br>`
-    );
-  } else {
-    document.write(
-      `${pokemon.name}: ${pokemon.height} meters tall. It is a medium-sized Pokemon. <br>`
-    );
-  }
+  pokemonRepository.addListItem(pokemon);
 });
+//if (pokemon.height >= 1.5) {
+//   document.write(
+//     `${pokemon.name}: ${pokemon.height} meters tall. It is a big Pokemon. <br>`
+//   );
+// } else if (pokemon.height <= 0.5) {
+//   document.write(
+//     `${pokemon.name}: ${pokemon.height} meters tall. It is a small Pokemon. <br>`
+//   );
+// } else {
+//   document.write(
+//     `${pokemon.name}: ${pokemon.height} meters tall. It is a medium-sized Pokemon. <br>`
+//   );
+// }
